ba5f8be05286ab92a70533d8c21af8599b11e419
Sachintha Lakmin <68807141+sachintha-lk@users.noreply.github.com>
2024-04-25 15:10:56 +0530

    Added Bootstrap and Custom input fields

---
+++ src/Components/Input/TextInput.js
@@ -0,0 +1,8 @@
+import React from "react";
+import CustomInput from "./CustomInput";
+
+export default function TextInput({...props}) {
+  return (
+    <CustomInput {...props}></CustomInput>
+  );
+}
