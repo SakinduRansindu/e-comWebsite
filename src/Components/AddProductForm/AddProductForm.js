1c2c46e28a43a40684325f56eaa32586b7440f56
Pramudi02 <pramupiyumika@gmail.com>
2024-05-01 12:31:54 +0530

    'add-product-form'

--- src/Components/AddProductForm/AddProductForm.js
+++ src/Components/AddProductForm/AddProductForm.js
@@ -4,7 +4,44 @@ export default function AddProductForm() {
   return (
     <form className='container-fluid'>
         <h1>Add Product</h1>
-
+        <div className="input-group">
+        <label for="">Prodouct ID</label> <br/>
+        <input type="text" />
+    </div>
+    <div className="input-group">
+        <label for="">Product Category</label> <br/>
+        <input type="text" />
+    </div>
+    <div className="input-group">
+        <label for="">Available Units</label> <br/>
+        <input type="text" />
+    </div>
+    <div className="input-group">
+        <label for="">Display Name</label> <br/>
+        <input type="text" />
+    </div>
+    <div className="input-group">
+        <label for="">Description</label> <br/>
+        <input type="text" />
+    </div>
+    <div className="input-group">
+        <label for="">Unit Price</label> <br/>
+        <input type="text" />
+    </div>
+    <div className="input-group">
+        <label for="">Discount</label> <br/>
+        <input type="text" />
+    </div>
+    <div className="input-group">
+        <label for="">Discount End Date</label> <br/>
+        <input type="text" />
+    </div>
+    <div className="input-group">
+        <label for="">Images</label> <br/>
+        <input type="file" />
+    </div>
+        
     </form>
   )
 }
+
