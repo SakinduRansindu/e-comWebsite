b0bb44ca04b39a5ddbcd3aa236a819beff4097ac
garuka-satharasinghe <142331771+garuka-satharasinghe@users.noreply.github.com>
2024-05-05 23:44:57 +0530

    updated footer

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -8,12 +8,7 @@ export default function Footer({ ...props }) {
           <ul class="nav justify-content-center border-bottom pb-3 mb-3">
             <li class="nav-item">
               <a href="#" class="nav-link px-2 text-body-secondary">
-                Home
-              </a>
-            </li>
-            <li class="nav-item">
-              <a href="#" class="nav-link px-2 text-body-secondary">
-                About
+                Back to Top
               </a>
             </li>
           </ul>
