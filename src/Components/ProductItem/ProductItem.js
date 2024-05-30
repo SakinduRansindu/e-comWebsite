23a73480add7c699519a18ff21d745f1f0a3def1
garuka-satharasinghe <142331771+garuka-satharasinghe@users.noreply.github.com>
2024-05-30 17:17:48 +0530

    updated product cards

--- src/Components/ProductItem/ProductItem.js
+++ src/Components/ProductItem/ProductItem.js
@@ -4,31 +4,42 @@ import Button from "../../button.js";
 
 export default function ProductItem({ ...props }) {
   return (
-    <div className="container-fluid col">
-      <div className="p-3 m-4 mx-sm-4 mx-md-3 mx-lg-2 border rounded card">
+    <div className="col">
+      <div
+        className="card h-100 border-0"
+        style={{ backgroundColor: "#1A2029" }}
+      >
         <Carousel
+          className="card-img-top"
           Cid={props.id}
           Imgs={props.Imgs}
           style={null}
           onClick={null}
         ></Carousel>
         <div className="card-body d-flex flex-column">
-          <h3 className="card-title">{props.title}</h3>
-
-          <p className="card-text">{props.description}</p>
-
-          <h4 className="card-text text-white text-bold">LKR {props.price}</h4>
-          {props.isDiscountApplied && (
-            <span className="text-bold text-body-secondary display-inline">
-              <del>LKR {props.priceBeforeDiscount}</del>
+          <h5 className="card-title">{props.title}</h5>
+          <div className="d-flex align-items-center">
+            <p className="card-text mb-0  text-white fs-4 mr-2">
+              LKR {props.price}
+            </p>
+            <span className="text-bold text-body-secondary display-inline m-2">
+              {props.isDiscountApplied && (
+                <del>LKR {props.priceBeforeDiscount}</del>
+              )}
             </span>
-          )}
+          </div>
+
           {props.availableUnits !== undefined && (
             <p className="card-text text-body-secondary">
               {props.availableUnits === 0 ? (
-                <small className="text-danger text-bold">Out of Stock</small>
+                <small className="badge text-wrap text-bg-danger">
+                  Out of Stock
+                </small>
               ) : (
-                <small>
+                <small
+                  class="badge text-wrap text-black"
+                  style={{ backgroundColor: "#548AFF" }}
+                >
                   {props.availableUnits < 5
                     ? `Only ${props.availableUnits} Units Left`
                     : `${props.availableUnits} Units Left`}
@@ -36,20 +47,24 @@ export default function ProductItem({ ...props }) {
               )}
             </p>
           )}
+        </div>
+        <div className="card-body d-flex align-items-end">
           <div className="py-0 flex-column">
-            <p className="card-text mb-1">
-              <small class="text-bold text-body-secondary">by {props.seller}</small>
-            </p>
-
             <a href={props.productUrl}>
-              { props.availableUnits && props.availableUnits > 0 ? (
+              {props.availableUnits && props.availableUnits > 0 ? (
                 <Button label="Buy Now" className="btn btn-primary m-0" />
               ) : (
                 <Button label="View" className="btn btn-primary m-0" />
-
-              )}              
+              )}
             </a>
           </div>
+        </div>
+        <div className="card-footer">
+          <p className="card-text mb-1">
+            <small class="text-bold text-body-secondary">
+              by {props.seller}
+            </small>
+          </p>
         </div>
       </div>
     </div>
--- src/button.js
+++ src/button.js
@@ -1,24 +1,20 @@
-import React from 'react';
+import React from "react";
 
 const Button = ({ label, onClick, disabled, variant = "primary" }) => {
   // Define button styles based on variant
   const buttonStyles = {
     primary: {
-      backgroundColor: '#121f31',
-      color: 'white',
-      padding: '10px 20px',
-      borderRadius: '5px',
-      border: '1px solid #0a58ca',
-      cursor: 'pointer',
+      backgroundColor: "#121f31",
+      color: "white",
+      padding: "10px 20px",
+      borderRadius: "5px",
+      border: "1px solid #0a58ca",
+      cursor: "pointer",
     },
-    
   };
 
   return (
-    <button
-      disabled={disabled}
-      style={buttonStyles[variant]}
-    >
+    <button disabled={disabled} style={buttonStyles[variant]}>
       {label}
     </button>
   );
