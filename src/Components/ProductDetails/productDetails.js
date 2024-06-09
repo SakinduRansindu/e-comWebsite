fcc8871c50d55920df0880928522988367a6eaa0
Tharin Edirisinghe <tharinedirisinghe10@gmail.com>
2024-06-09 16:36:37 +0530

    productDetails responsive mode ON

--- src/Components/ProductDetails/productDetails.js
+++ src/Components/ProductDetails/productDetails.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import Carousel from "../Carousel/Carousel";
 import { GetProductDetails } from "../../API/ProductsApi";
 import Alert from "../Alert/Alert";
-
+import { useNavigate } from "react-router-dom";
 
 const ProductDetails = ({ pid, minimalData = false }) => {
   const [product, setProduct] = useState({});
@@ -23,7 +23,9 @@ const ProductDetails = ({ pid, minimalData = false }) => {
 
   useEffect(() => {
     if (!minimalData && product?.ProductImgs) {
-      const tmpImgs = product.ProductImgs.map((img) => `${process.env.REACT_APP_BASE_URL}/uploads/` + img.imgUrl);
+      const tmpImgs = product.ProductImgs.map(
+        (img) => `${process.env.REACT_APP_BASE_URL}/uploads/` + img.imgUrl
+      );
       setImgs(tmpImgs);
       setState("loaded");
     } else if (minimalData && product?.DisplayName) {
@@ -31,140 +33,97 @@ const ProductDetails = ({ pid, minimalData = false }) => {
     }
   }, [product, minimalData]);
 
-  const styles = {
-    container: {
-      display: 'flex',
-      flexDirection: 'column',
-      alignItems: 'center',
-      margin: '0 auto',
-      padding: '20px',
-      maxWidth: '1200px',
-    },
-    productDetails: {
-      display: 'flex',
-      flexDirection: 'row',
-      gap: '20px',
-      marginTop: '20px',
-      width: '100%',
-      border: '2px solid #ddd',
-      borderRadius: '10px', 
-      padding: '20px', 
-      
-      
-    },
-    productImage: {
-      flex: 1,
-      display: 'flex',
-      justifyContent: 'center',
-      alignItems: 'center',
-    },
-    productInfo: {
-      flex: 1,
-      display: 'flex',
-      flexDirection: 'column',
-      gap: '10px',
-    },
-    productName: {
-      fontSize: '2rem',
-      fontWeight: 'bold',
-      marginBottom: '10px',
-      color: 'white',
-    },
-    productPrice: {
-      fontSize: '1.5rem',
-      color: '#FF0000',
-    },
-    strike: {
-      textDecoration: 'line-through',
-      color: '#666',
-      fontSize: '1rem',
-    },
-    productCategory: {
-      fontSize: '1.2rem',
-      color: '#ddd',
-    },
-    productUnits: {
-      fontSize: '1.2rem',
-      color: '#ddd',
-    },
-    productOutOfStock: {
-      fontSize: '1.2rem',
-      color: 'red',
-    },
-    specifications: {
-      marginTop: '20px',
-    },
-    productDescriptionTitle: {
-      fontSize: '1.5rem',
-      marginBottom: '10px',
-      color: '#ddd',
-    },
-    productDescription: {
-      fontSize: '1.2rem',
-      color: '#666',
-    },
-    button: {
-      padding: '8px 18px',
-      fontSize: '1rem',
-      border: 'none',
-      borderRadius: '5px',
-      cursor: 'pointer',
-      backgroundColor: '#007bff',
-      color: 'white',
-    },
-    buttonDisabled: {
-      backgroundColor: '#cccccc',
-      cursor: 'not-allowed',
-    },
-    card: {
-      border: '1px solid #ddd',
-      borderRadius: '5px',
-      padding: '20px',
-      marginTop: '20px',
-      boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)',
-      backgroundColor: '#1b4676',
-      width: '50%',
-      margin: '0 auto',
-    },
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    navigate(`/payments/${product.ProductId}`);
   };
 
-  if (!minimalData && state === 'loaded') {
+  if (!minimalData && state === "loaded") {
     return (
-      <div style={styles.container}>
-        <div style={styles.productDetails}>
-          <div style={styles.productImage}>
-            <Carousel Imgs={imgs} style={{ maxHeight: '80vw' }} Cid=""></Carousel>
-          </div>
-          <div style={styles.productInfo}>
-            <h2 style={styles.productName}>{product.DisplayName}</h2>
-            <p style={styles.productPrice}>
-              Price: Rs.{product.UnitPrice * (100 - product.Discount) / 100} <span style={styles.strike}>Rs.{product.UnitPrice}</span>
-            </p>
-            <p style={styles.productCategory}>Category: {product.Category}</p>
-            {product.AvailableUnits > 0 ? (
-              <p style={styles.productUnits}>Available Units: {product.AvailableUnits}</p>
-            ) : (
-              <p style={styles.productOutOfStock}>Out of Stock</p>
-            )}
-            <div style={styles.specifications}>
-              <h3 style={styles.productDescriptionTitle}>Description:</h3>
-              <p style={styles.productDescription}>{product.Description}</p>
-              <a href={`/payments/${product.ProductId}`}>
-                <button
-                  disabled={product.AvailableUnits === 0}
-                  style={product.AvailableUnits === 0 ? { ...styles.button, ...styles.buttonDisabled } : styles.button}
-                >
-                  Buy Now
-                </button>
-              </a>
-            </div>
+      <section class="py-5">
+        <div class="container">
+          <div class="row gx-5">
+            <aside class="col-lg-6">
+              <div class=" mb-3 d-flex justify-content-center">
+                <Carousel Imgs={imgs} Cid=""></Carousel>
+              </div>
+            </aside>
+            <main class="col-lg-6">
+              <div class="ps-lg-3">
+                <h2 class="title text-light">{product.DisplayName}</h2>
+                <div>
+                  <span className="h6">Category: </span>
+                  <span>{product.Category}</span>
+                  <dd class="col-1"></dd>
+                </div>
+                <div class="d-flex flex-row my-3">
+                  <span class="text-muted">
+                    <span class="text-body-secondary ">
+                      {product.AvailableUnits > 0 ? (
+                        <p
+                          className="badge fw-medium fs-6 text-muted"
+                          style={{
+                            backgroundColor: "#1A2029",
+                          }}
+                        >
+                          Available Units: {product.AvailableUnits}
+                        </p>
+                      ) : (
+                        <p className="badge fw-medium fs-6 text-bg-danger">
+                          Out of Stock
+                        </p>
+                      )}
+                    </span>
+                  </span>
+                </div>
+
+                <div class="rounded p-4" style={{ backgroundColor: "#1A2029" }}>
+                  <p className="text-muted">Description:</p>
+                  <p>{product.Description}</p>
+                </div>
+
+                <div class="my-4">
+                  <span class="pe-2 display-4">
+                    LKR {(product.UnitPrice * (100 - product.Discount)) / 100}
+                  </span>
+
+                  <span class="text-muted fs-5">
+                      <strike>LKR {product.UnitPrice}</strike>
+                  </span>
+                </div>
+                <div class="d-flex flex-row ">
+                  <button
+                    disabled={product.AvailableUnits === 0}
+                    className="btn btn-light add-to-cart px-5"
+                    onClick={handleClick}
+                    style={{ backgroundColor: "#84b0e2" }}
+                  >
+                    Buy Now
+                  </button>
+
+                  <button
+                    onClick={() =>
+                      alert(
+                        `${product.DisplayName} has been added to your cart!`
+                      )
+                    }
+                    disabled={product.AvailableUnits === 0}
+                    className="btn add-to-cart ms-3"
+                    style={{ backgroundColor: "#2C384D" }}
+                  >
+                    Add to Cart
+                  </button>
+                </div>
+              </div>
+            </main>
           </div>
         </div>
-      </div>
+      </section>
     );
   } else if (minimalData && state === "loaded") {
     return (
-      <div style={styles.card}>
+      <div className="card w-50 mx-auto bg-success">
         <div className="container m-1">
           <h4>{product.DisplayName}</h4>
           <p>Discount: {product.Discount}%</p>
